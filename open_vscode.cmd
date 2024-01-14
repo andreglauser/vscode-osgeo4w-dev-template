d0a55cfab9b455013566c9d619b135af10446d2e
André Glauser <mail@andreglauser.com>
2024-01-14 11:50:38 +0100

    Fix for QGIS 3.28 LTR

--- open_vscode.cmd
+++ open_vscode.cmd
@@ -1,18 +1,18 @@
 @echo off
-SET OSGEO4W_ROOT=C:\OSGeo4W64
+SET OSGEO4W_ROOT=C:\OSGeo4W
+SET PYTHON_VERSION=Python39
+
 call "%OSGEO4W_ROOT%"\bin\o4w_env.bat
-call "%OSGEO4W_ROOT%"\bin\qt5_env.bat
-call "%OSGEO4W_ROOT%"\bin\py3_env.bat
 
 path %PATH%;%OSGEO4W_ROOT%\apps\qgis-ltr\bin
 path %PATH%;%OSGEO4W_ROOT%\apps\Qt5\bin
-path %PATH%;%OSGEO4W_ROOT%\apps\Python37\Scripts
+path %PATH%;%OSGEO4W_ROOT%\apps\%PYTHON_VERSION%\Scripts
 
 set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis-ltr\python\
 set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis-ltr\python\qgis
 set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis-ltr\python\qgis\PyQt5
 set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis-ltr\python\qgis\core
 
-set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python37
+set PYTHONHOME=%OSGEO4W_ROOT%\apps\%PYTHON_VERSION%
 
 start "VS Code with PyQGIS and OsGeo4W" /B "%LOCALAPPDATA%\Programs\Microsoft VS Code\Code.exe" .
