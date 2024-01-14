21f8121195e7940b26ffdc67cb921f8d26cc2904
André Glauser <mail@andreglauser.com>
2024-01-14 11:51:07 +0100

    Also check for gdal and ogr

--- example_python.py
+++ example_python.py
@@ -1,8 +1,36 @@
-import qgis.core
-import PyQt5
 
-qgis_version = qgis.core.Qgis.QGIS_VERSION
-print('QGIS {} is available'.format(qgis_version))
 
-qt_version = PyQt5.QtCore.QT_VERSION_STR
-print('PyQt5 {} is available'.format(qt_version))
+
+def main():
+    try:
+        import qgis.core
+        qgis_version = qgis.core.Qgis.QGIS_VERSION
+        print("QGIS {} is available".format(qgis_version))
+    except ImportError:
+        print("Import of QGIS failed")
+
+    try:
+        import PyQt5
+        qt_version = PyQt5.QtCore.QT_VERSION_STR
+        print("PyQt5 {} is available".format(qt_version))
+    except ImportError:
+        print("Import of PyQt5 failed")
+
+    try:
+        from osgeo import gdal
+
+        gdal_version = gdal.__version__
+        print("GDAL {} is available".format(gdal_version))
+    except ImportError:
+        print("Import of GDAL from OSGEO failed")
+
+    try:
+        from osgeo import ogr
+
+        print("OGR is available")
+    except ImportError:
+        print("Import of OGR from OSGEO failed")
+
+
+if __name__ == "__main__":
+    main()
